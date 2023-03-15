1aa7991a9fd390c832825ac386c1ffe9e81165f4
sippakon khammisawang <sippakon_k@cmu.ac.th>
2023-03-15 13:09:21 +0700

    full

--- lab25_1.cpp
+++ lab25_1.cpp
@@ -63,4 +63,23 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx){
+    size--;
+	if(idx==0)
+	{
+		Node *current = root;
+		root = root->next;
+		delete current;
+	}
+	else 
+	{
+		Node *current = root;
+		for(int i=0;i < idx-1;i++)
+		{
+			current = current->next;
+		}
+		Node *T = current->next;
+		current->next = current->next->next;
+		delete T;
+	}
+}
--- lab25_3.cpp
+++ lab25_3.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-//Write function count() here.
+int count(int a[],int b)
+{
+    set<int> mySet;
+    for(int j=0;j<b;j++)
+    {
+        mySet.insert(a[j]);
+    }
+    return mySet.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
